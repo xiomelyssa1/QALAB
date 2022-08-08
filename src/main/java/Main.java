0eb234b11690798ef8451f06b215d9216df78c29
Xiomelys Mairen Yssa Hernandez <xyssaher@emeal.nttdata.com>
2022-08-08 17:07:40 -0300

    proyectobase

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String args[]) {
+ System.out.println("Hola, Bienvenidos a Java!!");
+ System.out.println("Hola a todos!!!");
+
+ int edad = 25; // => Declaración e Inicialización
+ edad = edad + 30; // => Asignación (cambio de valor + operación
+
+  String nombre;  // Declaración
+  nombre = "Xio";  // Asignación
+
+   int edad1 = 10, edad2 = 20, edad3 = 30;
+
+
+ System.out.println("La edad es: " + edad);
+ System.out.println(nombre);
+    }
+}
