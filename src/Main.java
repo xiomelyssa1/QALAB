1b6aefb4f9d07e39415d2a9e7ad4a7735adfc020
Xiomelys Mairen Yssa Hernandez <xyssaher@emeal.nttdata.com>
2022-08-08 17:26:51 -0300

    Solicitud de Claves

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        String abc = "Raul Fernandez!";
+        System.out.println(abc.substring(0,1).compareTo("R"));
+
+
+        }
+    }
+
+
