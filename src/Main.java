cca669a6ce8573b90b1b13d3cea3fa441615348a
xiomelyssa1 <110853321+xiomelyssa1@users.noreply.github.com>
2022-08-08 17:30:48 -0300

    Cambio desde interfaz web

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String abc = "Raul Fernandez!";
+        String abc = "Raul Fernandez, Bienvenido!!!";
         System.out.println(abc.substring(0,1).compareTo("R"));
 
 
